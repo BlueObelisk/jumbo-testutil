12be31c01c3b952d1f2f1d741d79cdfc33c096ba
Jim Downing <jimdowning@pobox.com>
2010-01-19 12:59:20 +0000

    Improved XML node tracking for attributes and text nodes

--- src/main/java/org/xmlcml/cml/testutil/TestUtils.java
+++ src/main/java/org/xmlcml/cml/testutil/TestUtils.java
@@ -105,7 +105,8 @@ public final class TestUtils implements CMLConstants {
 			Assert.assertEquals(message + ": classes", testNode.getClass(),
 					refNode.getClass());
 			if (refNode instanceof Text) {
-				testStringDoubleEquality(message, refNode.getValue(), testNode
+				testStringDoubleEquality(message + " on node: "
+						+ path(testNode), refNode.getValue(), testNode
 						.getValue(), eps);
 			} else if (refNode instanceof Comment) {
 				Assert.assertEquals(message + " comment", refNode.getValue(),
@@ -144,7 +145,8 @@ public final class TestUtils implements CMLConstants {
 						Assert.fail(message + " attribute on ref not on test: "
 								+ attName);
 					}
-					testStringDoubleEquality(message, refAtt.getValue(),
+					testStringDoubleEquality(message + " on node: "
+							+ path(testNode), refAtt.getValue(),
 							testAtt.getValue(), eps);
 				}
 			} else {
